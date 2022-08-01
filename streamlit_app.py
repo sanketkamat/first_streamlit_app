d8a9e3b87e8e54fff3774966129ada728112b7de
sanketkamat <110359032+sanketkamat@users.noreply.github.com>
2022-08-01 14:40:42 -0400

    Update streamlit_app.py
    add pandas lib
    add fruit list from aws

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas as pd
 
 streamlit.title("My Mom's New Healthy Diner")
 
@@ -10,3 +11,4 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
                 
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
