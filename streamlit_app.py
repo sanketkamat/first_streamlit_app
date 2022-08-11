08b3800ad39d5257e88d917a92099dada1d0af3d
sanketkamat <110359032+sanketkamat@users.noreply.github.com>
2022-08-11 09:33:29 -0400

    Update streamlit_app.py
    creates table format of json data

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,4 +26,11 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json()) #just writes data to the screen
+
+# take the json version of data and normalize it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# creates table format of json data
+streamlit.dataframe(fruityvice_normalized)
+
+
